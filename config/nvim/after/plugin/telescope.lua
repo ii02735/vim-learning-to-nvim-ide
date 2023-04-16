3984872e2bdedeed72d1dd76e4ca242123d2bffd
ii02735 <yadallee.bilaal@outlook.fr>
2023-04-16 23:43:47 +0200

    Update telescope binding

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -2,10 +2,8 @@
 
 local builtin = require('telescope.builtin')
 -- To find files
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = '[S]earch [F]iles'})
 -- To find files sourced by Git (less cumbersome : avoid useless directories like node_modules or vendor for example)
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gp', builtin.git_files, { desc = '[S]earch [F]iles tracked by [G]it'})
 -- To find files that has a specific content (grep search like)
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>P', builtin.live_grep, { desc = '[S]earch by [G]rep' })
